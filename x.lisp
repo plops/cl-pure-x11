de389da857ecc3059864cf5a8bd639da9c260090
m <martin@uffz.(none)>
2010-10-31 16:07:21 +0000

    compiles now, deleted unnecessary things

--- x.lisp
+++ x.lisp
@@ -2,6 +2,14 @@
   (:use :cl :sb-bsd-sockets))
 (in-package :x)
 
+(defparameter *s* nil)
+(defparameter *resp* nil)
+(defparameter *root* nil)
+(defparameter *window* nil)
+(defparameter *gc* nil)
+(defparameter *resource-id-base* nil)
+(defparameter *resource-id-mask* nil)
+
 (defun connect ()
   (defparameter *s*
     (socket-make-stream
@@ -161,42 +169,14 @@
 					    'class class
 					    'colormap-entries colormap-entries))))))))))))
 
-(parse-initial-response *resp*)
-
-(let ((success (= 1 (aref *resp* 0)))
-      (maximum-request-length (+ (ash (aref *resp* 7) 8)
-				 (aref *resp* 6))))
-  success)
-; x11r7proto.pdf p.123 describes request formats
-(let* ((create-gc-list '(55 ; opcode
-			 0 ; unused
-			 5 0 ; length
-			 0 0 #xc2 2 ; cid
-			 #x17 1 0 0 ; drawable
-			 8 0 0 0 ; value-mask background
-			 255 255 255 0 ; background
-
-			 ;; 20 ; opcode get-property
-			 ;; 0 ; delete
-			 ;; 6 0 ; length
-			 ;; #x17 1 0 0 ; window
-			 ;; 23 0 0 0 ; resource-manager
-			 ;; 31 0 0 0 ; get-property-type string
-			 ;; 0 0 0 0 ; offset
-			 ;; 0 #xe1 #xf5 5 ; length
-			 )))
-  (write-sequence 
-   (make-array (length create-gc-list)
-	       :element-type '(unsigned-byte 8)
-	       :initial-contents create-gc-list)
-   *s*)
-  (force-output *s*))
-
+#+nil
 (progn
   (connect)
   (parse-initial-response *resp*)
   (make-window)
   (draw-window))
+
+; x11r7proto.pdf p.123 describes request formats
 
 (defun make-window ()
  (let* ((l ()) ;; first byte of request gets pushed in first
@@ -293,47 +273,3 @@
        (force-output *s*)
        ))))
 
-
-(let* ((create-win-list '(1    ; opcode
-			  0   ; depth
-			  13 0 ; length
-			  #x0d 0 1 3 ; wid
-			  #x17 1 0 0; parent
-			  0 0 ; x
-			  0 0 ; y
-			  1 0 ; w
-			  1 0 ; h
-			  1 0 ; border-width
-			  1 0 ; input-output window-class
-			  0 0 0 0 ; copy-from-parent
-			  #x1a #x28 0 0 ; value-mask
-			  255 255 255 0 ; bg pixel
-			  0 0 0 0 ; border pixel
-			  1 ; bit-gravity north-west
-			  0 0 0 ; unused
-			  0 0 #x62 0 ; event-mask
-			  #x20 0 0 0 ; colormap
-			  ))
-       (create-window (make-array (length create-win-list)
-				  :element-type '(unsigned-byte 8)
-				  :initial-contents create-win-list)))
-  (write-sequence create-window *s*)
-  (force-output *s*))
-
-#+nil
-((major-op-code 1
-length-in-4-bytes 2
-data 1)
-create-window->opcode=1
- wid 8
-parent 8 #x117
-class 1
-depth 1
-visual-id 8
-x 2
-y 2
-width 2
-height 2
-border-width 2
-value-mask ?
-value-list n*4)
