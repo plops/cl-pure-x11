f2e6c3bcff3bfea0c177418e5d77849f578fb88c
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-06-21 09:12:21 +0200

    figure out how to map the unix domain socket to tcp

---
+++ obj.lisp
@@ -0,0 +1,15 @@
+(ql:quickload :defclass-std)
+(ql:quickload :pure-x11)
+
+(defpackage :g
+  (:use :cl :pure-x11))
+(in-package :g)
+
+;; nowadays X servers normally don't listen on port 6000
+;; make it work without restarting
+;; socat -d -d TCP-LISTEN:6000,fork,bind=localhost UNIX-CONNECT:/tmp/.X11-unix/X0
+
+(progn ;; open a window and draw a line
+  (connect)
+  (make-window)
+  (draw-window 0 0 100 100))
