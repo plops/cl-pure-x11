e8eb261e68747d17f4a88ddbdbbb4d6fdb89808e
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-06-21 09:13:18 +0200

    add reference

--- obj.lisp
+++ obj.lisp
@@ -8,6 +8,7 @@
 ;; nowadays X servers normally don't listen on port 6000
 ;; make it work without restarting
 ;; socat -d -d TCP-LISTEN:6000,fork,bind=localhost UNIX-CONNECT:/tmp/.X11-unix/X0
+;; https://askubuntu.com/questions/41330/let-xorg-listen-on-tcp-but-only-to-localhost
 
 (progn ;; open a window and draw a line
   (connect)
